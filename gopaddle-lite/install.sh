8442937ac78089f19b29d240699c554eccbd5a20
renugadevi-2613 <renugadevi@bluemeric.com>
2022-05-06 19:29:01 +0530

    installer script modified

--- gopaddle-lite/install.sh
+++ gopaddle-lite/install.sh
@@ -10,7 +10,7 @@ helm install gp-rabbitmq-4-2 gp-lite/gp-lite  --namespace gp-lite  --set global.
 
 sleep 1m
 
-helm install gp-core-4-2 gp-lite/gp-lite  --namespace gp-lite  --set global.routingType=NodePortWithOutIngress --set global.installType=public --set global.storageClassName=microk8s-hostpath --set global.cluster.provider=other --set-string global.gopaddle.https=false --set-string global.gopaddleWebhook.https=false --set global.staticIP=$STATICIP  --set global.gp-rabbitmq.enabled=false --set global.gp-lite-core.enabled=true --set gateway.gpkubeux.INSTALL_SOURCE=civo --version 4.2.2
+helm install gp-core-4-2 gp-lite/gp-lite  --namespace gp-lite  --set global.routingType=NodePortWithOutIngress --set global.installType=public --set global.storageClassName=microk8s-hostpath --set global.cluster.provider=other --set-string global.gopaddle.https=false --set-string global.gopaddleWebhook.https=false --set global.staticIP=$STATICIP  --set global.gp-rabbitmq.enabled=false --set global.gp-lite-core.enabled=true --set gateway.gpkubeux.installSource=civo --version 4.2.2
 
 sleep 2m
  
